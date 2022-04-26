03684b27a67ae666900732e890858c3e8606f3d4
rubenpain <ruben@appsolute.fr>
2022-04-26 22:07:44 +0200

    [APP] add and use cookie-parser in app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { Client } = require("pg");
 const user = require("./routes/user");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const localhost = "127.0.0.1";
@@ -16,6 +17,7 @@ const client = new Client({
 client.connect();
 
 app.use("/user", user);
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "./assets/template/html/index.html");
