3cb0f8ac68eb8a4ac2f63353fc2460be378d97a1
rubenpain <ruben@appsolute.fr>
2022-04-26 20:34:06 +0200

    [FUNCTION] create a middleware to implement on all routes where we need auth

---
+++ middleware/authentication.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+class AuthenticationMiddleware{
+    authentication (req, res, next) {
+        try {
+            const token = req.header("x-auth-token");
+            if (!token) return res.status(403).send("Access denied.");
+    
+            const decoded = jwt.verify(token, process.env.JWTPRIVATEKEY);
+            req.user = decoded;
+            next();
+        } catch (error) {
+            res.status(400).send("Invalid token");
+        }
+    };
+}
+
+module.exports = AuthenticationMiddleware();
