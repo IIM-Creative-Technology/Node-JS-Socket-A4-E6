619495b0de765acb2d697f61054bc683f06d1342
rubenpain <ruben@appsolute.fr>
2022-04-26 09:03:32 +0200

    add db connection in index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const { Client } = require('pg');
 const express = require('express')
 const app = express()
 const port = 3000
+const client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+})
+
+client.connect()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
 })
 
 app.listen(port, () => {
