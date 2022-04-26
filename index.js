e2c5001597b465fc4ce2167e2fe2d0ba074a38d1
Allan <allan.duche@gmail.com>
2022-04-26 10:55:02 +0200

    [Routing] Refactoring + added ;

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const localhost = '127.0.0.1'
 const port = 3000;
 
 const user = require('./routes/user');
@@ -8,8 +9,8 @@ app.use('/user', user);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
+    console.log(`Example app listening on : http://${localhost}:${port}`);
+});
