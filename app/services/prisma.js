d1d95d10b084073807b9951cd7c467df09238e25
rubenpain <ruben@appsolute.fr>
2022-04-26 20:45:04 +0200

    [REFACTO] remove some db code

--- app/services/prisma.js
+++ app/services/prisma.js
@@ -1,15 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
-const { Client } = require("pg");
 
 const prisma = new PrismaClient();
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-client.connect();
 
 async function main() {
   const allUsers = await prisma.user.findMany();
