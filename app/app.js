29c1bd13e0f178f2c9ab4a14202e7e6d4db9d68a
julesgabriel <julesgabriel.dayaux@gmail.com>
2022-04-27 12:33:06 +0200

    ADD alert on connexion to room

--- app/app.js
+++ app/app.js
@@ -8,21 +8,17 @@ const user = require("./routes/user");
 
 io.on("connection", (socket) => {
   console.log(`Connecté au client ${socket.id}`)
-  socket.on("joinRoom",  (roomName) => {
-    socket.join(roomName);
-    io.sockets.in(roomName).emit('message','Someone joined the room');
+  socket.on("joinRoom",  () => {
+    socket.join("room1");
+    io.sockets.in("room1").emit('message','Someone joined the room');
   })
 });
 
 app.use("/user", user);
 
-
-
-
 app.get("/socket", (req, res) => {
   res.sendFile(__dirname + "/socketio/index.html");
 });
-
 
 httpServer.listen(3000);
 
