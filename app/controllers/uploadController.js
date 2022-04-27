2d3e424cb380810d2aea5698217ca256da95b79b
yannyiim <yanny.ouzid@edu.devinci.fr>
2022-04-27 16:37:14 +0200

    add pdf uploading

--- app/controllers/uploadController.js
+++ app/controllers/uploadController.js
@@ -6,7 +6,7 @@ var storage = multer.diskStorage({
         cb(null, "uploads")
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now()+".jpg")
+        cb(null, Date.now() + "-" + file.originalname)
     }
   })
 
