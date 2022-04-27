baf198d030eef858a315955d31d4ea40d9d65c75
FairFightKing <sosthenefruchard@gmail.com>
2022-04-27 11:40:51 +0200

    add environnements variables
    # Conflicts:
    #	app/app.js
    #	package.json

--- app/app.js
+++ app/app.js
@@ -1,4 +1,36 @@
 const express = require("express");
+require('dotenv').config()
+const pg = require("pg");
+const fs = require('fs');
+const postgresqlUri = process.env.DATABASE_URL
+const conn = new URL(postgresqlUri);
+conn.search = conn.query = "";
+const config = {
+  connectionString: conn.href,
+  ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync('./app/ca.pem').toString(),
+  },
+};
+
+const client = new pg.Client(config);
+client.connect(function (err) {
+    if (err)
+        throw err;
+    client.query("SELECT * FROM test", [], function (err, result) {
+        if (err)
+            throw err;
+
+        console.log("Successfully connected");
+        client.end(function (err) {
+            if (err)
+                throw err;
+        });
+    });
+});
+
+const user = require("./routes/user");
+
 const app = express();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
