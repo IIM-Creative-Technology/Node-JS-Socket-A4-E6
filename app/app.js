da417ae2c65cabe4748943c758e441023d04f303
yannyiim <yanny.ouzid@edu.devinci.fr>
2022-04-28 13:47:14 +0200

    rebase with develop

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const app = express();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 const user = require("./routes/user");
@@ -12,14 +12,7 @@ const path = require("path")
 
 
 io.on("connection", (socket) => {
-  socket.on("joinRoom",  () => {
-    socket.join("room1");
-    io.sockets.in("room1").emit('welcome', {msg: `${socket.id} joined the room`, socketId: socket.id})
-    // io.sockets.adapter.rooms Lister les rooms
-  })
-  socket.on('room1 chat message', function (values) {
-    io.to("room1").emit('room callback', {...values, room: "room1"});
-  });
+  console.log(`Connecté au client ${socket.id}`);
 });
 
 app.use("/user", user);
@@ -27,9 +20,15 @@ app.use("/upload", uploadRoutes);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/upload/index.html");
 });
+io.on("connection", (socket) => {
+  socket.on("hello", (arg) => {
+    console.log(arg); // world
+  });
+});
 
 app.get("/socket", (req, res) => {
   res.sendFile(__dirname + "/socketio/index.html");
 });
 
-httpServer.listen(process.env.EXPRESS_PORT || 3000);
+httpServer.listen(3000);
+
---
+++ app/controllers/uploadController.js
@@ -0,0 +1,46 @@
+const path = require("path")
+const multer = require("multer")
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now()+".jpg")
+    }
+  })
+
+  const maxSize = 1 * 1000 * 1000;
+
+  var upload = multer({ 
+    storage: storage,
+    limits: { fileSize: maxSize },
+    fileFilter: function (req, file, cb){
+    
+        var filetypes = /jpeg|jpg|png|pdf/;
+        var mimetype = filetypes.test(file.mimetype);
+  
+        var extname = filetypes.test(path.extname(
+                    file.originalname).toLowerCase());
+        
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+      
+        cb("Error: File upload only supports the "
+                + "following filetypes - " + filetypes);
+      } 
+  
+// mypic is the name of file attribute
+}).single("mypic"); 
+
+exports.uploadFiles = function (req, res) {
+    upload(req,res,function(err) {
+        if(err) {
+            res.send(err)
+        }
+        else {
+            res.send("Success, Image uploaded!")
+        }
+    })
+  };
